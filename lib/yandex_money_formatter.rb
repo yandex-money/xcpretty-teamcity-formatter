6bc1c6b131b009925c209d5d74ce25ccecf01174
zalutskii <zalutskii@yamoney.ru>
2018-12-28 15:35:12 +0300

    Release 1.0.8

--- lib/yandex_money_formatter.rb
+++ lib/yandex_money_formatter.rb
@@ -1,55 +1,90 @@
-
 class TeamCityFormatter < XCPretty::Simple
-	
-	def initialize (use_unicode, colorize)
-		super
-		STDOUT.puts("##teamcity[compilationStarted compiler='xcodebuild']")
-		at_exit do
-			STDOUT.puts("##teamcity[compilationFinished compiler='xcodebuild']")
-		end
-	end
-	
-	# Errors and warnings.
-	def format_compile_error(file_name, file_path, reason, line, cursor)
-		STDOUT.puts("##teamcity[message text='Compile error' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
-	def format_error(message)
-		STDOUT.puts("##teamcity[message text='Error' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
-	def format_file_missing_error(error, file_path)
-		STDOUT.puts("##teamcity[message text='Missing file' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
-	def format_ld_warning(message)
-		STDOUT.puts("##teamcity[message text='LD warning' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
-	def format_undefined_symbols(message, symbol, reference)
-		STDOUT.puts("##teamcity[message text='Undefined symbols' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
-	def format_duplicate_symbols(message, file_paths)
-		STDOUT.puts("##teamcity[message text='Duplicate symbols' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
-	def format_warning(message)
-		STDOUT.puts("##teamcity[message text='Warning' errorDetails='#{super}' status='WARNING']\n")
-		super
-	end
-	
-	def format_compile_warning(file_name, file_path, reason, line, cursor)
-		STDOUT.puts("##teamcity[message text='Compile warning' errorDetails='#{super}' status='ERROR']\n")
-		super
-	end
-	
+  def initialize(use_unicode, colorize)
+    super
+    log compile_group(false)
+    at_exit do
+      log compile_group(true)
+    end
+  end
+
+  # Errors and warnings.
+  def format_compile_error(_, file_path, reason, line, cursor)
+    details = "#{file_path}: #{reason}\n#{line}\n#{cursor}"
+    compile_error('Compile error', details)
+  end
+
+  def format_error(message)
+    compile_error('Error', super)
+  end
+
+  def format_file_missing_error(error, file_path)
+    compile_error('Error', "#{file_path}: #{error}")
+  end
+
+  def format_ld_warning(message)
+    compile_error('LD warning', super)
+  end
+
+  def format_undefined_symbols(message, symbol, reference)
+    details = "#{message}\n" \
+      "> Symbol: #{symbol}\n" \
+      "> Referenced from: #{reference}"
+    compile_error('Undefined symbols', details)
+  end
+
+  def format_duplicate_symbols(message, file_paths)
+    details = "#{message}" \
+      "> #{file_paths.join("\n> ")}"
+    compile_error('Duplicate symbols', details)
+  end
+
+  def format_warning(message)
+    compile_error('Warning', message)
+  end
+
+  def format_compile_warning(_, file_path, reason, line, cursor)
+    details = "#{file_path}: #{reason}\n#{line}\n#{cursor}"
+    compile_error('Compile warning', details)
+  end
+
+  private
+
+  # @param [String] str
+  def log(str)
+    STDOUT.puts str
+  end
+
+  # @param [String] message
+  # @param [String] unformatted_details
+  # @param [Bool] warning
+  # @return [String]
+  def compile_error(message, unformatted_details, warning = false)
+    status = warning ? 'WARNING' : 'ERROR'
+    str    = "##teamcity[message text='#{message}' "
+    unless warning
+      error_details = format_error_details(unformatted_details)
+      str           += "errorDetails='#{error_details}' "
+    end
+    str + "status='#{status}']\n"
+  end
+
+  # @param [Bool] finished
+  # @return [String]
+  def compile_group(finished)
+    command = finished ? 'compilationFinished' : 'compilationStarted'
+    "##teamcity[#{command} compiler='xcodebuild']"
+  end
+
+  # @param [String] str
+  # @return [String]
+  def format_error_details(str)
+    str.gsub('|', '||')
+       .gsub("\n", '|n')
+       .gsub("'", "|'")
+       .gsub('[', '|[')
+       .gsub(']', '|]')
+
+  end
 end
 
 TeamCityFormatter
--- xcpretty-yandex-money-formatter.gemspec
+++ xcpretty-yandex-money-formatter.gemspec
@@ -1,28 +1,22 @@
-# coding: utf-8
-
 Gem::Specification.new do |spec|
-  spec.name          = "xcpretty-yandex-money-formatter"
-  spec.version       = "1.0.1"
-  spec.authors       = ["Alxander Zalutskiy"]
-  spec.email         = ["zalutskii@yamoney.ru"]
+  spec.name          = 'xcpretty-yandex-money-formatter'
+  spec.version       = '1.0.8'
+  spec.authors       = ['Alxander Zalutskiy']
+  spec.email         = ['zalutskii@yamoney.ru']
   spec.description   =
-  %q{
-  Formatter for xcpretty customized to provide pretty output on TeamCity
-  }
-  spec.summary       = %q{xcpretty custom formatter for TeamCity}
-  spec.homepage      = "https://github.com/yandex-money/xcpretty-teamcity-formatter"
-  spec.license       = "MIT"
-  spec.required_ruby_version = "~> 2.0"
-  spec.files         = [
-  	"README.md",
-  	"LICENSE",
-  	"lib/yandex_money_formatter.rb",
-  	"bin/xcpretty-yandex-money-formatter"]
-  spec.executables   = ["xcpretty-yandex-money-formatter"]
-  spec.require_paths = ["lib"]
-  spec.add_dependency "xcpretty", "~> 0.2", ">= 0.0.7"
+    '
+    Formatter for xcpretty customized to provide pretty output on TeamCity
+    '
+  spec.summary       = 'xcpretty custom formatter for TeamCity'
+  spec.homepage      = 'https://github.com/yandex-money/xcpretty-teamcity-formatter'
+  spec.license       = 'MIT'
+  spec.required_ruby_version = '~> 2.2'
+  spec.files         = %w[README.md LICENSE lib/yandex_money_formatter.rb bin/xcpretty-yandex-money-formatter]
+  spec.executables   = ['xcpretty-yandex-money-formatter']
+  spec.require_paths = ['lib']
+  spec.add_dependency 'xcpretty', '~> 0.2', '>= 0.0.7'
 
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "bacon", "~> 1.2"
+  spec.add_development_dependency 'bacon', '~> 1.2'
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
 end
